9d63b1e433c9a9c3e17adf70cc1a375a6267dc6c
Petkov Petar <petar.petkov@doka.com>
2020-05-04 20:09:31 +0400

    Add project files.

---
+++ CurveChordSplitter/CurveChordSplitterComponent.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Geometry;
+using Grasshopper.Kernel.Types;
+using Grasshopper.Kernel.Types.Transforms;
+using Rhino.Geometry;
+using Rhino.Geometry.Intersect;
+using CurveChordSplitter.Properties;
+//using System.Linq;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace CurveChordSplitter
+{
+    public class CurveChordSplitterComponent : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public CurveChordSplitterComponent()
+          : base("CurveChordSplitter", "CurveChordSplitter",
+              "Curve Split By Chords. Returns points and parameters",
+              "Curve", "Analysis")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddCurveParameter("Curve", "C", "curve to be split", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Chords", "C", "Chords to split by", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddPointParameter("Points", "P", "P", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Parameters", "T", "T", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Curve curve = Curve.CreateControlPointCurve(new List<Point3d>(), 0);
+            List<double> chords = new List<double>();
+                
+            if (!DA.GetData(0, ref curve)) return;
+            if (!DA.GetDataList(1, chords)) return;
+
+            //var kkk = GH_Point.;
+            var outputPoints = new List<Point3d>();
+            var outputParameters = new List<double>();
+            GetPoints(curve, ref chords, ref outputPoints, ref outputParameters);
+
+
+            DA.SetDataList(0, outputPoints);
+            DA.SetDataList(1, outputParameters);
+        }
+
+        private void GetPoints(Curve curve, ref List<double> radiuses, ref List<Point3d> output, ref List<double> outputParameters)
+        {
+            //add the begining of the curve as output point
+            output.Add(curve.PointAtStart);
+            //create circle at start of the curve with the first available radius
+            if (radiuses.Count > 0)
+            {
+                var circle = new Circle(curve.PointAtStart, radiuses.ElementAt<double>(0));
+                radiuses.RemoveAt(0);
+                //create temp curcle var of rhino type because intersection does not know how to use circle
+                Curve circleAsGHCurve = null;
+                GH_Convert.ToCurve(new GH_Circle(circle), ref circleAsGHCurve, GH_Conversion.Both);
+                //intersect circle with curve
+                var intersection = Rhino.Geometry.Intersect.Intersection.CurveCurve(circleAsGHCurve, curve, 0.0, 0.0);
+
+                if (intersection.Count > 0)
+                {
+                    //create split parameter from the point
+                    var splitPointParameter = intersection[0].ParameterB;
+                    outputParameters.Add(splitPointParameter);
+                    //split the curve on the first point
+                    var splitCurvesArray = curve.Split(splitPointParameter);
+
+                    GetPoints(splitCurvesArray[1], ref radiuses, ref output, ref outputParameters);
+                }
+            }
+            return;
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //var icon = new Bitmap("CurveChordSplitter");
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+
+                return Resources.CurveChordSplitterIcon; 
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("4b0f66d9-3119-4f19-93f8-c2b894e2f840"); }
+        }
+    }
+}
---
+++ CurveChordSplitter/CurveChordSplitterInfo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace CurveChordSplitter
+{
+    public class CurveChordSplitterInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Curve Split By Chords";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                // var icon = new Bitmap("CurveChordSplitter");
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "Curve Split By Chords. Returns points and parameters";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("cfc0a892-c2a5-46c5-a65f-b071f00f438c");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "Petar Petkov";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "petar.b.petkov@gmail.com";
+            }
+        }
+    }
+}
